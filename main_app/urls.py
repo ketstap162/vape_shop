c9538a144f07c12bf67d81aff39aeaf564449fb7
Zakhar Podporinov <zp162162@gmail.com>
2023-08-15 20:17:17 +0300

    Create base.html and connect static folder

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from main_app.views import home_page_view
 
 app_name = "MainApp"
 
 urlpatterns = [
+    path("", home_page_view, name="home")
 ]
--- new_app/urls.py
+++ new_app/urls.py
@@ -21,7 +21,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("main_app.urls"), name="MainApp")
+    path("", include("main_app.urls"))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

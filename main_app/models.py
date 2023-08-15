295f73fe470b6617d47b6348b4e81ab085b48155
Zakhar Podporinov <zp162162@gmail.com>
2023-08-15 17:02:29 +0300

    Create database structure

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Section(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    image = models.ImageField(null=True, blank=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    section_id = models.ForeignKey(Section, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    category_id = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)

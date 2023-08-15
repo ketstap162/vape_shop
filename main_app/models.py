f8f741cda35017bea9203e98dd42acfcced0ff94
Zakhar Podporinov <zp162162@gmail.com>
2023-08-15 17:21:30 +0300

    Create image path functions

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,26 @@
+import os
+
 from django.db import models
+
+
+def get_file_name(model_name, filename):
+    _, extension = os.path.splitext(filename)
+    return "_".join(model_name.split()).lower() + extension
+
+
+def image_path_section(model_instance, filename):
+    filename = get_file_name(model_instance.name, filename)
+    return os.path.join("uploads/sections/", filename)
+
+
+def image_path_product(model_instance, filename):
+    filename = get_file_name(model_instance.name, filename)
+    return os.path.join("uploads/products/", filename)
 
 
 class Section(models.Model):
     name = models.CharField(max_length=40, unique=True)
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to=image_path_section)
 
 
 class Category(models.Model):
@@ -14,5 +31,5 @@ class Category(models.Model):
 class Product(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    image = models.ImageField(null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, upload_to=image_path_product)
     category_id = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)

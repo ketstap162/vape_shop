a0f018272bbcb4183f6d05450f1ebaedf41f53d7
Zakhar Podporinov <zp162162@gmail.com>
2023-08-17 15:36:22 +0300

    Add images and data in DB + bug fixes

--- main_app/models.py
+++ main_app/models.py
@@ -33,6 +33,9 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
 
+    def name_without_spaces(self):
+        return self.name.replace(" ", "_")
+
     def __str__(self):
         return self.name
 
--- main_app/views.py
+++ main_app/views.py
@@ -5,7 +5,15 @@ from main_app.models import Section, Category, Product
 def home_page_view(request):
     context = {
         "sections": Section.objects.prefetch_related("category_set"),
-        "products": Product.objects.select_related("category_id")
+        "products": Product.objects.select_related("category_id__section_id")
     }
-    print(vars(context["sections"][0]))
+
+    if request.GET.get("section"):
+        query_filter = request.GET.get("section").replace("_", " ")
+        context["products"] = context["products"].filter(category_id__section_id__name=query_filter)
+
+    if request.GET.get("category"):
+        query_filter = request.GET.get("category").replace("_", " ")
+        context["products"] = context["products"].filter(category_id__name=query_filter)
+
     return render(request, "base.html", context=context)
